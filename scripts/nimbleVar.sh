c525d6dc9a12e0eac9f22fc0adce95572e743291
Jake Leahy <jake@leahy.dev>
2025-12-29 17:55:49 +1100

    Investigate the nimble dump

--- scripts/nimbleVar.sh
+++ scripts/nimbleVar.sh
@@ -2,4 +2,7 @@
 #
 # This generates a list of variables you can source which map to variables in a .nimble file
 # Everything is prefixed with `nimble_`
+set -e
+set -o pipefail
+
 nimble dump | sed -E "s/(\w+): \"(.*)\"/nimble_\\1=\\2/" | source
