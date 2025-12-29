32f3dac1474f5910f9e730d0b9eb00b27dbeb8cc
Jake Leahy <jake@leahy.dev>
2025-12-29 18:06:29 +1100

    Source the file correctly
    We want to use the output

--- scripts/nimbleVar.sh
+++ scripts/nimbleVar.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 #
 # This generates a list of variables you can source which map to variables in a .nimble file
 # Everything is prefixed with `nimble_`
 set -e
 set -o pipefail
 
-nimble dump | sed -E "s/(\w+): \"(.*)\"/nimble_\\1=\\2/" | source
+nimble dump | sed -E "s/(\w+): \"(.*)\"/nimble_\\1=\\2/"
